df646144ec4efe7b94e880cfb72d285061497753
Laknath1996 <ashwindesilva@yahoo.com>
2019-05-12 19:51:22 +0530

    Update

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 '''
 execute DRIUNet
+
+work to complete
+
+Read the papers
+Modify the architectures
+Read on the accuracy metrics F1, sensitivity etc
+Use the full scale images
+Don't use a validation set
+Look in to optical disk segmentation too
+
 '''
 
 from src.DRIUNet import *
@@ -11,13 +21,13 @@ from src.utilities import *
 import scikitplot as skplt
 
 cwd = os.getcwd()
-data_path = os.path.join(cwd, 'datasets/DRIVE_2.hdf5')
-model_path = os.path.join(cwd, 'models/model1.hdf5')
+data_path = os.path.join(cwd, 'datasets/DRIVE_3.hdf5')
+model_path = os.path.join(cwd, 'models/model3.hdf5')
 
-# save_to_hdf5(os.path.join(cwd, 'datasets'), 'DRIVE_2.hdf5')
+# save_to_hdf5(os.path.join(cwd, 'datasets'), 'DRIVE_3.hdf5')
 
 deepRetina = DRIUNET(data_path, model_path)
-# deepRetina.train()
+deepRetina.train()
 deepRetina.predict()
 
 
