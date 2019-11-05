ee4505ccef4afdb2c1dfed0cebef2ca25bcb1939
Laknath1996 <ashwindesilva@yahoo.com>
2019-11-05 07:45:19 +0530

    Update

--- main.py
+++ main.py
@@ -12,18 +12,61 @@ Look in to optical disk segmentation too
 
 '''
 
-from src.DRIUNet import *
-from src.utilities import *
+"""
+objective : Execute
+author(s) : Ashwin de Silva
+date      : 
+"""
 
-cwd = os.getcwd()
-data_path = os.path.join(cwd, 'datasets/DRIVE_3.hdf5')
-model_path = os.path.join(cwd, 'models/model.hdf5')
+from src.sclera_segmentation.utilities import save_to_hdf5, plot_samples
+import matplotlib.pyplot as plt
+import h5py
+import numpy as np
+from src.sclera_segmentation.ScleraNet import *
 
-# save_to_hdf5(os.path.join(cwd, 'datasets'), 'DRIVE_3.hdf5')
+#########################################
+# save to hdf5
+#########################################
 
-deepRetina = DRIUNET(data_path, model_path)
-# deepRetina.train()
-# deepRetina.predict()
+# imageset_path = '/Volumes/Seagate Expansion Drive/SBVP_with_masks'
+# save_to_hdf5(imageset_path, size=(256, 256))
+
+#########################################
+# visualize
+#########################################
+
+# data_path = '/Users/ashwin/DeepRetina/src/sclera_segmentation/data/scleral_dataset'
+# data = h5py.File(data_path, mode='r')
+# x_train = np.array(data["val"])
+# y_train = np.array(data["val_mask"])
+#
+# y_train[y_train >= 0.5] = 255
+# y_train[y_train < 0.5] = 0
+#
+# plot_samples(x_train[10], y_train[10])
+
+#########################################
+# train
+#########################################
+
+data_path = '/home/ashwind/DeepRetina/src/sclera_segmentation/data/scleral_dataset'
+model_path = '/home/ashwind/DeepRetina/src/sclera_segmentation/models/sclera_model.hdf5'
+
+scn = ScleraNet(data_path, model_path)
+scn.train()
+
+# from src.DRIUNet import *
+# from src.utilities import *
+#
+# cwd = os.getcwd()
+# data_path = os.path.join(cwd, 'datasets/DRIVE_3.hdf5')
+# model_path = os.path.join(cwd, 'models/model.hdf5')
+#
+# # save_to_hdf5(os.path.join(cwd, 'datasets'), 'DRIVE_3.hdf5')
+#
+# deepRetina = DRIUNET(data_path, model_path)
+# # deepRetina.train()
+# # deepRetina.predict()
 
 
 
