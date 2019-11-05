f92a55e72d36b627d3e77a05eb9a9a85bdee8cae
Laknath1996 <ashwindesilva@yahoo.com>
2019-11-05 07:39:04 +0530

    Update

--- main.py
+++ main.py
@@ -17,13 +17,13 @@ from src.utilities import *
 
 cwd = os.getcwd()
 data_path = os.path.join(cwd, 'datasets/DRIVE_3.hdf5')
-model_path = os.path.join(cwd, 'models/model3.hdf5')
+model_path = os.path.join(cwd, 'models/model.hdf5')
 
 # save_to_hdf5(os.path.join(cwd, 'datasets'), 'DRIVE_3.hdf5')
 
 deepRetina = DRIUNET(data_path, model_path)
-deepRetina.train()
-deepRetina.predict()
+# deepRetina.train()
+# deepRetina.predict()
 
 
 
